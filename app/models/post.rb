68dda6127c02168820acfea354e1b1ee1a2103a7
Taylor Williams <taylor@thewilliams.net>
2018-11-12 01:48:54 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: :true
   validates :content, length: {minimum: 50}
   validates :summary, length: {maximum: 50}
-  validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :title, format: {with: /Won't Believe|Secret|Top \d|Guess/}
 end
