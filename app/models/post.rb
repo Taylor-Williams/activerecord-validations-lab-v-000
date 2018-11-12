08fed8e0348da9a705c9962a6e42ab82085b1ee8
Taylor Williams <taylor@thewilliams.net>
2018-11-12 01:34:19 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  validates title:, presence: :true
-  validates content:, length: {minimum: 50}
+  validates :title, presence: :true
+  validates :content, length: {minimum: 50}
 end
